e5e1e0df778f99e0c02957fbf7f6691804701165
Dyachkov Yaroslav <Xan-Yar0slav@yandex.ru>
2024-10-04 22:03:41 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var elem := ReadInteger('Введите год:');
+  Assert(elem > 0);
+  if (a mod 4 = 0) and (a mod 100 <> 0) or (a mod 400 = 0) then 
+    Print('True') 
+  else Print('False');
+end.
