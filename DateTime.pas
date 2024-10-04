4e0be18240987995a9b6af769c84d926f7befef6
Dyachkov Yaroslav <Xan-Yar0slav@yandex.ru>
2024-10-04 22:39:01 +0300

    Add what day is closer to New Yaer

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,12 @@
 ﻿begin
   var elem := ReadInteger('Введите год:');
   Assert(elem > 0);
-  if (a mod 4 = 0) and (a mod 100 <> 0) or (a mod 400 = 0) then 
-    Print('True') 
-  else Print('False');
+  PrintLn($'Год високосный: {(elem mod 4 = 0) and (elem mod 100 <> 0) or (elem mod 400 = 0)}');
+  
+  var (day1, month1, day2, month2) := ReadInteger4('Введите две даты:');
+  if month1 > month2 then Print(1)
+  else if month1 < month2 then Print(2)
+  else if day1 > day2 then Print(1)
+  else if day1 < day2 then Print(2)
+  else Print('Даты одинаковые')
 end.
